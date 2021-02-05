38f6f43280c6c607119809ee885793cb0bf03d1e
yunuskoken <06yunuskoken@gmail.com>
2021-02-05 23:12:11 +0300

    Add project files.

---
+++ DataAcces/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,36 @@
+﻿using DataAcces.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAcces.Concrete.EntityFramework
+{
+    public class EfProductDal : IProductDal
+    {
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return new List<Product> { new Product { ProductName = "Tablo" }, new Product { ProductName = "Su" } };
+        }
+
+        public List<Product> GetAllCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
